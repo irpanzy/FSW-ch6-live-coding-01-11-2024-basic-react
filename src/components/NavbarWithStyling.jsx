a4d78ab01b74a1db331c7dc6c0e3b0ceb95f7d31
pasipasipada <irfanoputra27@gmail.com>
2024-11-02 10:25:57 +0700

    first commit

---
+++ src/components/NavbarWithStyling.jsx
@@ -0,0 +1,16 @@
+import "./NavbarWithStyling.css";
+
+function NavbarWithStyling() {
+  return (
+    <div>
+      <h1>Hello FSW 2</h1>
+      <ul>
+        <li>About Me</li>
+        <li>FAQ</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavbarWithStyling;
